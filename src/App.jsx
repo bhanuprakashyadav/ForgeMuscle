e96af01098fbfda5a3833adf662f286b9f6b7d26
Bhanu Prakash Yadav <bhanuprakashyadav909@gmail.com>
2024-10-06 00:12:49 +0530

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
-import WhoWeAre from "./Components/Who we are/whoWeAre"
-import AllClasses from "./Components/All classes/AllClasses"
-import WhyUs from "./Components/Why Choose Us/WhyUs"
+import AllClasses from "./Components/Allclasses/AllClasses"
+import WhyUs from "./Components/WhyChooseUs/WhyUs"
 import Footer from "./Components/Footer/Footer"
+import WhoWeAre from "./Components/Whoweare/WhoWeAre"
 
 function App() {
 
